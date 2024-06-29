42ced5d286146f85a3eaf932af2bb790a6bcdcc4
Rishabh Mittal <85026429+Rishabh416@users.noreply.github.com>
2024-06-29 19:43:52 +0400

    fixed window clipping issue

--- project.js
+++ project.js
@@ -3,8 +3,8 @@ setDocDimensions(width, 125)
 const minBuildingHeight = 15
 const maxBuildingHeight = 77
 const spacingDistance = 9
-const buildingWidth = 20
-const windowSize = 7
+const buildingWidth = 35
+const windowSize = 4
 bt.setRandSeed(872)
 
 const pencil = new bt.Turtle();
@@ -33,11 +33,11 @@ function door(buildingNo) {
 }
 
 function window(buildingNo, height) {
-  const windowNoRow = Math.floor(buildingWidth / (windowSize + 1));
+  const windowNoRow = Math.floor(buildingWidth / (windowSize + 1)-0.5);
   const windowNoCol = Math.floor((height - 7) / (windowSize + 3));
   for (let j = 0; j < windowNoCol; j++) {
     for (let i = 0; i < windowNoRow; i++) {
-      const gap = ((buildingWidth % windowSize)/2)-0.5
+      const gap = ((buildingWidth % windowSize)/2)+2
       pencil.jump([(buildingNo * spacingDistance) + ((buildingNo - 1) * buildingWidth) + gap + (i*windowSize) + (i*1), 10+(3*j)+(j*windowSize)])
       pencil.left(90)
       pencil.forward(windowSize)
